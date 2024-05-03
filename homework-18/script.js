c72b95f5d837d4c9916400d40750937fd91f662c
stella <lisyukstella0@gmail.com>
2024-05-03 14:52:37 +0300

    add appendChild and createElement instead of innerHTML

--- homework-18/script.js
+++ homework-18/script.js
@@ -2,15 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('http://api.openweathermap.org/data/2.5/weather?q=Kyiv&units=metric&APPID=5d066958a60d315387d9492393935c19')
         .then(response => response.json())
         .then(data => {
-            let weatherInfo = '<p>Температура: ' + Math.round(data.main.temp) + '°C</p>';
-            weatherInfo += '<p>Тиск: ' + data.main.pressure + ' hPa</p>';
-            weatherInfo += '<p>Опис: ' + data.weather[0].description + '</p>';
-            weatherInfo += '<p>Вологість: ' + data.main.humidity + '%</p>';
-            weatherInfo += '<p>Швидкість вітру: ' + data.wind.speed + ' м/с</p>';
-            weatherInfo += '<p>Напрям вітру: ' + data.wind.deg + '°</p>';
-            weatherInfo += '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="Погодний значок">';
-            
-            document.getElementById('weather').innerHTML = weatherInfo;
+            let temperatureParagraph = document.createElement('p');
+            temperatureParagraph.textContent = 'Температура: ' + Math.round(data.main.temp) + '°C';
+            document.getElementById('weather').appendChild(temperatureParagraph);
+
+            let pressureParagraph = document.createElement('p');
+            pressureParagraph.textContent = 'Тиск: ' + data.main.pressure + ' hPa';
+            document.getElementById('weather').appendChild(pressureParagraph);
+
+            let descriptionParagraph = document.createElement('p');
+            descriptionParagraph.textContent = 'Опис: ' + data.weather[0].description;
+            document.getElementById('weather').appendChild(descriptionParagraph);
+
+            let humidityParagraph = document.createElement('p');
+            humidityParagraph.textContent = 'Вологість: ' + data.main.humidity + '%';
+            document.getElementById('weather').appendChild(humidityParagraph);
+
+            let windSpeedParagraph = document.createElement('p');
+            windSpeedParagraph.textContent = 'Швидкість вітру: ' + data.wind.speed + ' м/с';
+            document.getElementById('weather').appendChild(windSpeedParagraph);
+
+            let windDirectionParagraph = document.createElement('p');
+            windDirectionParagraph.textContent = 'Напрям вітру: ' + data.wind.deg + '°';
+            document.getElementById('weather').appendChild(windDirectionParagraph);
+
+            let weatherIcon = document.createElement('img');
+            weatherIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
+            weatherIcon.setAttribute('alt', 'Погодний значок');
+            document.getElementById('weather').appendChild(weatherIcon);
         })
         .catch(error => console.error('Помилка при отриманні даних з сервера:', error));
 });
